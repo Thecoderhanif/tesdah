ec71d7c4076da7429550062927bdec797a4b796a
unknown <usahabaru2002@gmail.com>
2022-01-16 23:48:37 +0700

    first commit

--- components/Container.js
+++
@@ -1,9 +0,0 @@
-import { Flex } from '@chakra-ui/react';
-
-export default function Container({ children }) {
-  return (
-    <Flex flexDir="column" justifyContent="center" pt="2" pb="5">
-      {children}
-    </Flex>
-  );
-}
